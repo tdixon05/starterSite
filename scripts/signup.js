ded9d5128e55a598af8359a2ba6262fc45512a89
Trevonte Dixon <trevontedixon@Trevontes-MacBook-Pro.local>
2025-01-13 20:31:05 -0500

    updated signup page

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,12 +1,9 @@
-// Import Firebase configuration and database modules
 import { app } from './firebase/firebase-config.js';
 import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js';
 
-// Initialize the database
 const database = getDatabase(app);
 
-// Handle form submission
-document.querySelector('form').addEventListener('submit', function (event) {
+document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault();
 
     const emailInput = document.getElementById('email').value;
@@ -16,7 +13,7 @@ document.querySelector('form').addEventListener('submit', function (event) {
         push(emailRef, { email: emailInput })
             .then(() => {
                 alert('Thank you for signing up!');
-                document.getElementById('email').value = ''; // Clear the input field
+                document.getElementById('email').value = '';
             })
             .catch((error) => {
                 console.error('Error saving data:', error);
